5be0e36e4cec53af25b1f88def7c6512b76fcea5
goosebomb <as379907as@gmail.com>
2024-07-23 16:40:06 +0900

    fix: restore file

---
+++ outbound_delivery_robot/src/outbound_delivery_robot_movement/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'outbound_delivery_robot_movement'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hb',
+    maintainer_email='rowl126@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'astar_movement = outbound_delivery_robot_movement.astar_movement:main',
+        'command_publisher = outbound_delivery_robot_movement.command_publisher:main',
+        ],
+    },
+)
