ef0a3c2ccce2780a6038d20dc8bda728136e3d17
goosebomb <as379907as@gmail.com>
2024-07-23 16:41:08 +0900

    fix: blueprint added

--- outbound_delivery_server/odServer.py
+++ outbound_delivery_server/odServer.py
@@ -3,6 +3,7 @@ from router.manager import manager
 from router.reader import reader
 from router.led import led
 from router.camera import camera
+from router.location import location
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.register_blueprint(manager, url_prefix='/manager')
 app.register_blueprint(reader, url_prefix='/tag')
 app.register_blueprint(led, url_prefix='/led')
 app.register_blueprint(camera, url_prefix='/camera')
+app.register_blueprint(location, url_prefix='/location')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
