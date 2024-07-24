6021665a5cc84451b3760f135242d88163c37070
hyun <rowl126@gmail.com>
2024-07-24 21:06:22 +0900

    fix: Requests update

--- outbound_delivery_robot/src/outbound_delivery_robot_tag_reader/rfid_reader/rfid_publisher_node.py
+++ outbound_delivery_robot/src/outbound_delivery_robot_tag_reader/rfid_reader/rfid_publisher_node.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
+import requests
 
 class RFIDPublisher(Node):
     def __init__(self):
@@ -10,6 +11,7 @@ class RFIDPublisher(Node):
         self.publisher_ = self.create_publisher(String, 'rfid_data', 10)
         self.reader = SimpleMFRC522()
         self.timer = self.create_timer(1.0, self.publish_rfid_data)
+        self.server_url = "http://192.168.0.79:5000/product/tag"
 
     def publish_rfid_data(self):
         try:
@@ -18,8 +20,23 @@ class RFIDPublisher(Node):
             msg = String()
             msg.data = f'ID: {id}, Text: {text}'
             self.publisher_.publish(msg)
+            
+            self.send_request_to_server(id, text)
         except Exception as e:
             self.get_logger().error(f'Error reading RFID: {e}')
+            
+    def send_request_to_server(self, id, text):
+        try:
+            payload = {'product' : text.strip()}
+            response = requests.post(self.server_url, params=payload)
+            if response.status_code == 200:
+                self.get_logger().info(f"Successfully sent data to server: {response.status_code}")
+                
+            else:
+                self.get_logger().error(f'Failed to send data to server : {response.status_code}')
+                
+        except Exception as e:
+            self.get_logger().error(f"Error sending request to server: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
@@ -30,7 +47,7 @@ def main(args=None):
         pass
     finally:
         rfid_publisher.destroy_node()
-        GPIO.cleanup()  # 프로그램 종료 시 GPIO.cleanup() 호출
+        GPIO.cleanup() 
         rclpy.shutdown()
 
 if __name__ == '__main__':
