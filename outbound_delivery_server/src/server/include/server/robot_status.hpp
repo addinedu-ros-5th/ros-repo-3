571b115dbc05d4cf5fdc98ffc1d9104dfa81846a
goosebomb <as379907as@gmail.com>
2024-07-25 21:36:41 +0900

    add: robot status

---
+++ outbound_delivery_server/src/server/include/server/robot_status.hpp
@@ -0,0 +1,13 @@
+enum class robotStatus
+{
+    CHARGING,
+    STANDBY,
+    MOVETOSECTION,
+    PICKING,
+    MOVETOPACKING,
+    PROCESS,
+    MOVETOOUTBOUND,
+    UNLOAD,
+    MOVETOSTATION,
+    PROBLEM
+};
