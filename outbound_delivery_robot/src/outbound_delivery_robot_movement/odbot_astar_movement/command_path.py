199009fce5dc4b38633baeda185cc614c56fc4d7
hyun <rowl126@gmail.com>
2024-07-25 16:23:06 +0900

    fix: Setting the initial pose

--- outbound_delivery_robot/src/outbound_delivery_robot_movement/odbot_astar_movement/command_path.py
+++ outbound_delivery_robot/src/outbound_delivery_robot_movement/odbot_astar_movement/command_path.py
@@ -3,21 +3,51 @@ from rclpy.node import Node
 from nav_msgs.msg import Path
 from nav2_simple_commander.robot_navigator import BasicNavigator, PoseStamped, TaskResult
 import math
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from std_msgs.msg import Header
+import time
 
 class PathFollower(Node):
     def __init__(self):
         super().__init__('path_follower')
+        
+        self.initial_pose_publisher = self.create_publisher(PoseWithCovarianceStamped, '/initialpose', 10)
+
+        
         self.subscription = self.create_subscription(Path, 'planned_path', self.path_callback, 10)
         self.nav = BasicNavigator()
         self.nav.waitUntilNav2Active()
+        
+        self.publish_initial_pose()
+        
+        time.sleep(2)
 
+    def publish_initial_pose(self):
+        pose_msg = PoseWithCovarianceStamped()
+        pose_msg.header = Header()
+        pose_msg.header.frame_id = 'map'
+        pose_msg.header.stamp = self.get_clock().now().to_msg()
+        
+        pose_msg.pose.pose.position.x = 0.03
+        pose_msg.pose.pose.position.y = -0.3
+        pose_msg.pose.pose.position.z = 0.0
+        pose_msg.pose.pose.orientation.x = 0.0
+        pose_msg.pose.pose.orientation.y = 0.0
+        pose_msg.pose.pose.orientation.z = 0.0
+        pose_msg.pose.pose.orientation.w = 1.0
+
+        pose_msg.pose.covariance = [0.0] * 36
+        
+        self.initial_pose_publisher.publish(pose_msg)
+        self.get_logger().info("Published initial pose")
+    
     def path_callback(self, msg):
         self.get_logger().info(f"Received path with {len(msg.poses)} points.")
 
-        
+        # 경로 따라가기
         self.nav.followWaypoints(msg.poses)
 
-        
+        # 경로 완료까지 대기
         while not self.nav.isTaskComplete():
             feedback = self.nav.getFeedback()
             if feedback:
@@ -33,7 +63,7 @@ class PathFollower(Node):
                         self.get_logger().info("Arrived at the Target")
                         break
 
-        
+        # 최종 결과 확인
         result = self.nav.getResult()
         if result == TaskResult.SUCCEEDED:
             self.get_logger().info("Reached the destination successfully!")
