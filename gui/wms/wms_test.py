256d23a98b63e758372ff89c6c5611b81ccb29e6
yohankim0423 <yohankim0423@gmail.com>
2024-07-29 22:08:33 +0900

    fix: wms test code file

--- gui/wms/wms_test.py
+++ gui/wms/wms_test.py
@@ -4,7 +4,7 @@ import yaml
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap, QTransform, QPainter, QPen
-from PyQt5.QtCore import Qt, QTimer, QPoint
+from PyQt5.QtCore import Qt, QTimer
 from threading import Thread
 
 import rclpy as rp
@@ -79,7 +79,7 @@ class AmclSubscriber(Node):
             ),
             'robot4': self.create_subscription(
                 PoseWithCovarianceStamped,
-                '/amcl_pose_4',  # 각 로봇의 고유 토픽 이름
+                '/amcl_pose_4',
                 self.amcl_callback_factory('robot4'),
                 amcl_pose_qos
             )
@@ -173,24 +173,6 @@ class WindowClass(QMainWindow, from_class):
         pos_x = (x - self.map_origin[0]) / self.map_resolution * self.image_scale
         pos_y = (y - self.map_origin[1]) / self.map_resolution * self.image_scale
         return int((self.width - pos_x)), int(pos_y)
-
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            x = event.position().x()
-            y = event.position().y()
-            self.set_robot_position(x, y)
-
-    def set_robot_position(self, x, y):
-        global robot_positions
-
-        map_x = (self.width - x) / self.image_scale * self.map_resolution + self.map_origin[0]
-        map_y = y / self.image_scale * self.map_resolution + self.map_origin[1]
-
-        # 로봇 이름 선택
-        robot_name = 'robot1'  # 여기서 임의로 'robot1'으로 설정, 필요에 따라 변경 가능
-
-        robot_positions[robot_name] = [map_x, map_y]
-        self.update_map()
 
     def closeEvent(self, event):
         rp.shutdown()  # ROS2 종료
