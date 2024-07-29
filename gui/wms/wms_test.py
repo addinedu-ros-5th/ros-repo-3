23be18069a1de9a265e55c89b9b9aba33e33fae6
yohankim0423 <yohankim0423@gmail.com>
2024-07-29 18:00:04 +0900

    test: robot positions

--- gui/wms/wms_test.py
+++ gui/wms/wms_test.py
@@ -4,7 +4,7 @@ import yaml
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap, QTransform, QPainter, QPen
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import Qt, QTimer, QPoint
 from threading import Thread
 
 import rclpy as rp
@@ -33,6 +33,10 @@ with open(yaml_file, 'r') as file:
 # 이미지 파일의 절대 경로를 설정합니다.
 image_path = os.path.join(current_dir, image_file)
 
+# 맵의 해상도와 원점 좌표 설정
+map_resolution = map_yaml_data['resolution']
+map_origin = map_yaml_data['origin'][:2]
+
 # 전역 변수 설정
 robot_positions = {
     'robot1': [-2.75, -1.6],  # 로봇 1의 초기 위치(x, y)
@@ -57,19 +61,19 @@ class AmclSubscriber(Node):
         self.subscribers = {
             'robot1': self.create_subscription(
                 PoseWithCovarianceStamped,
-                '/amcl_pose',  # 각 로봇의 고유 토픽 이름
+                '/amcl_pose_1',
                 self.amcl_callback_factory('robot1'),
                 amcl_pose_qos
             ),
             'robot2': self.create_subscription(
                 PoseWithCovarianceStamped,
-                '/amcl_pose_2',  # 각 로봇의 고유 토픽 이름
+                '/amcl_pose_2',
                 self.amcl_callback_factory('robot2'),
                 amcl_pose_qos
             ),
             'robot3': self.create_subscription(
                 PoseWithCovarianceStamped,
-                '/amcl_pose_3',  # 각 로봇의 고유 토픽 이름
+                '/amcl_pose_3',
                 self.amcl_callback_factory('robot3'),
                 amcl_pose_qos
             ),
@@ -169,6 +173,28 @@ class WindowClass(QMainWindow, from_class):
         pos_x = (x - self.map_origin[0]) / self.map_resolution * self.image_scale
         pos_y = (y - self.map_origin[1]) / self.map_resolution * self.image_scale
         return int((self.width - pos_x)), int(pos_y)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            x = event.position().x()
+            y = event.position().y()
+            self.set_robot_position(x, y)
+
+    def set_robot_position(self, x, y):
+        global robot_positions
+
+        map_x = (self.width - x) / self.image_scale * self.map_resolution + self.map_origin[0]
+        map_y = y / self.image_scale * self.map_resolution + self.map_origin[1]
+
+        # 로봇 이름 선택
+        robot_name = 'robot1'  # 여기서 임의로 'robot1'으로 설정, 필요에 따라 변경 가능
+
+        robot_positions[robot_name] = [map_x, map_y]
+        self.update_map()
+
+    def closeEvent(self, event):
+        rp.shutdown()  # ROS2 종료
+        super().closeEvent(event)  # 윈도우 종료
 
 def main():
     rp.init()
