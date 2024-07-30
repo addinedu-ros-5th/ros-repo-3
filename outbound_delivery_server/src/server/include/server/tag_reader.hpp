12f5d4df8847be606027252ef662f1fc7613231d
goosebomb <as379907as@gmail.com>
2024-07-30 13:34:09 +0900

    fix: change file name

---
+++ outbound_delivery_server/src/server/include/server/tag_reader.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <crow.h>
+#include <memory.h>
+#include <cppconn/driver.h>
+#include <cppconn/prepared_statement.h>
+#include <nlohmann/json.hpp>
+
+#include "database_connection.hpp"
+
+using json = nlohmann::json;
+
+class Outbound : public DatabaseConnection
+{
+    public:
+        Outbound(const std::string configFile): DatabaseConnection(configFile), bp("tag")
+        {
+            CROW_BP_ROUTE(bp, "outbound").methods(crow::HTTPMethod::POST)([this](const crow::request& req)
+            {
+                auto data = crow::json::load(req.body);
+                if (!data) { return crow::response(400, "Invalid JSON"); }
+                std::cout << data << std::endl;
+
+                auto status = data["tag_data"];
+
+                if (status == "Success")
+                {
+                    // TODO
+                }
+
+                return crow::response(200, "Publish successfully");
+            });
+
+            CROW_BP_ROUTE(bp, "picking").methods(crow::HTTPMethod::POST)([this](const crow::request& req)
+            {
+                auto data = crow::json::load(req.body);
+                if (!data) { return crow::response(400, "Invalid JSON"); }
+                std::cout << data << std::endl;
+
+                auto status = data["tag_data"];
+
+                if (status == "Success")
+                {
+                    // TODO
+                }
+
+                return crow::response(200, "Publish successfully");
+            });
+        }
+        
+
+        crow::Blueprint& getBlueprint()
+        {
+            return bp;
+        }
+
+    private:
+        crow::Blueprint bp;
+};
